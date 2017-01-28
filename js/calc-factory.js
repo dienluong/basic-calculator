1ac709ab7f41772373ebb3bafc6d5ddfb04621b0
Dien Luong <dysonlu@gmail.com>
2017-01-28 03:50:15 -0500

    Working version of calculator factory.

--- js/calc-factory.js
+++ js/calc-factory.js
@@ -1,118 +1,158 @@
-const calcProto = {
-  display() {},
-  operation() {},
-  plusminus() {},
-  decimal() {},
-  digit() {},
-  clearEntry() {},
-  clearAll() {}
-};
+// const calcProto = {
+//   display() {},
+//   operation() {},
+//   plusminus() {},
+//   decimal() {},
+//   digit() {},
+//   clearEntry() {},
+//   clearAll() {}
+// };
 
 
 function calcFactory(spec) {
-  const DISPLAY = '#display';
+  const DISPLAY = spec.display;
+  const BUTTONS = spec.buttons;
   let gEntriesArray  = [];
   let gHasDecimal    = false;
   let gLastNumber    = null;  // gLastNumber is null at startup and after CA and CE
   let gLastOp        = "";
   let gLastEntryType = "digit"; // digit or operation
 
-  let calcObj = Object.create(calcProto);
+  var Calculator = {
+    init () {
+      $(BUTTONS).on('click', this.buttonPressHandler.bind(this));
+    },
 
+    // Set or get display area text
+    updateDisplay (target, str) {
+      var container = $(target);
+      if (str === undefined)
+        return container.text() || "";
+      else
+        container.text(str);
+    },
 
-  // Set or get display area text
-  display(target, str) {
-    var container = $(target);
-    if (str === undefined)
-      return container.text() || "";
-    else
-      container.text(str);
-  }
+    operation (display, op) {
+      // if no digits entered previously, do nothing.
+      if (gLastNumber === null)
+        return display;
 
-  operationPressed(display) {
-    // if no digits entered previously, do nothing.
-    if (gLastNumber === null)
+      gLastOp = op;
+      gLastEntryType = 'operation';
       return display;
+    },
 
-    gLastOp = e.target.textContent;
-    gLastEntryType = 'operation';
-    return display;
-  }
+    plusminus (display) {
+      if (gLastNumber === null)
+        return display;
 
-  plusminusPressed(display) {
-    if (gLastNumber === null)
-      return display;
+      gLastNumber *= -1;
+      return display[0] === '-' ? display.slice(1): '-' + display;
+    },
 
-    gLastNumber *= -1;
-    return display[0] === '-' ? display.slice(1): '-' + display;
-  }
-
-  decimalPressed(display) {
-    // no current number
-    if (gLastNumber === null) {
-      display = "0.";
-      gHasDecimal = true;
-    }
-    else { // number has no decimal
-      if (!gHasDecimal) {
-        display = display + '.';
+    decimal (display) {
+      // no current number
+      if (gLastNumber === null) {
+        display = "0.";
         gHasDecimal = true;
       }
-      else { //if number ends with decimal, then toggle, otherwise do nothing
-        if (display[display.length-1] === '.') {
-          display = display.substr(0, display.length-1);
-          gHasDecimal = false;
+      else { // number has no decimal
+        if (!gHasDecimal) {
+          display = display + '.';
+          gHasDecimal = true;
+        }
+        else { //if number ends with decimal, then toggle, otherwise do nothing
+          if (display[display.length-1] === '.') {
+            display = display.substr(0, display.length-1);
+            gHasDecimal = false;
+          }
         }
       }
-    }
-    gLastNumber = Number(display);
-    gLastOp = "";
-    gLastEntryType = "digit";
-    return display;
-  }
-
-  digitPresssed(display) {
-    if (gLastEntryType === 'operation') {
-      if (gLastOp && gLastNumber !== null) {
-        gEntriesArray.push(gLastNumber);
-        gEntriesArray.push(gLastOp);
-      }
-      display = e.target.textContent;
-      gHasDecimal = false;
-    } // last entry was a digit so keep appending to number.
-    else {
-      if (Number(display) === 0 && !gHasDecimal)
-        //prevents leading zero
-        display = e.target.textContent;
-      else
-        display = display + e.target.textContent;
-    }
-
-    gLastNumber = Number(display);
-    gLastOp = "";
-    gLastEntryType = 'digit';
-    return display;
-  }
-
-  clearEntryPressed(display) {
-    if (gLastNumber !== null) {
-      gLastNumber = null;
-      gHasDecimal = false;
-      gLastEntryType = "digit";
+      gLastNumber = Number(display);
       gLastOp = "";
-      display = "";
+      gLastEntryType = "digit";
+      return display;
+    },
+
+    digit (display, dgt) {
+      if (gLastEntryType === 'operation') {
+        if (gLastOp && gLastNumber !== null) {
+          gEntriesArray.push(gLastNumber);
+          gEntriesArray.push(gLastOp);
+        }
+        display = dgt;
+        gHasDecimal = false;
+      } // last entry was a digit so keep appending to number.
+      else {
+        if (Number(display) === 0 && !gHasDecimal)
+          //prevents leading zero
+          display = dgt;
+        else
+          display = display + dgt;
+      }
+
+      gLastNumber = Number(display);
+      gLastOp = "";
+      gLastEntryType = 'digit';
+      return display;
+    },
+
+    clearEntry (display) {
+      if (gLastNumber !== null) {
+        gLastNumber = null;
+        gHasDecimal = false;
+        gLastEntryType = "digit";
+        gLastOp = "";
+      }
+      return ""; // returns "" as display
+    },
+
+    clearAll (display) {
+      if (gLastNumber !== null) {
+        gLastNumber = null;
+        gHasDecimal = false;
+        gLastEntryType = "digit";
+        gLastOp = "";
+      }
+      gEntriesArray = [];
+      return ""; // returns "" as display
+    },
+
+    buttonPressHandler (e) {
+        var nextDisplay = "";
+        var currentDisplay = this.updateDisplay(DISPLAY);
+        if (e.target.tagName === 'BUTTON') {
+          switch (e.target.textContent) {
+            case 'CE': nextDisplay = this.clearEntry(currentDisplay);
+                      break;
+            case 'AC': nextDisplay = this.clearAll(currentDisplay);
+                      break;
+            case '=': //calculate final result and display it
+                      break;
+            case '+': //addition
+            case '-': //substraction
+            case '\u00F7': //division
+            case '\u00D7': //multiplication
+                      nextDisplay = this.operation(currentDisplay, e.target.textContent);
+                      break;
+            case '\u00b1': //toggle +/-
+                      nextDisplay = this.plusminus(currentDisplay);
+                      break;
+            case '.': //decimal
+                      nextDisplay = this.decimal(currentDisplay);
+                      break;
+            default:  //last entry was an operation, then save the current number and operation
+                      nextDisplay = this.digit(currentDisplay, e.target.textContent);
+          }
+          console.log("Entries: ", gEntriesArray);
+          console.log("Last entry, number, op: ", gLastEntryType + " / " + gLastNumber + " / " + gLastOp);
+          this.updateDisplay(DISPLAY, nextDisplay);
+        }
+        e.stopPropagation();
     }
+  };
 
-    return display;
-  }
-
-  clearAllPressed(display) {
-    display = clearEntryPressed(display);
-    gEntriesArray = [];
-    return display;
-  }
-
-
-
+  var calcObj = Object.create(Calculator);
+  calcObj.init();
   return calcObj;
 }
